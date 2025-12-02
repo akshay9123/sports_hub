25538004c305167035b524f90cdabf83f292af48
akshay jha <akshayraj9123@gmail.com>
2025-12-02 17:43:21 +0530

    stock unit update

--- routes/stockUnit.routes.js
+++ routes/stockUnit.routes.js
@@ -15,6 +15,6 @@ router.get("/get_stock_unit", getAllStockUnits);
 router.get("/get_stock_unit_by_id/:id", getStockUnit);
 router.put("/update_stock_unit/:id", updateStockUnit);
 router.delete("/delete_stock_unit/:id", deleteStockUnit);
-router.get("/delete_stock_unit_by_code/:code", getStockUnitByCode);
+router.get("/get_stock_unit_by_code/:code", getStockUnitByCode);
 
 export default router;
